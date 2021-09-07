08d1f7e19ffe2c2995957d8526cff9e1df69c13b
Anupam Guin <anupam@etranssolutions.com>
2021-09-07 16:07:54 +0530

    Creating Shop Reducer & Directory Reducer and directory.selectors, directory components | using redux-persist to store cart data in local Storage

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,10 @@
-import React, { Component } from "react";
-import SHOP_DATA from "./shopHot.data";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-export class ShopPage extends Component {
-  constructor(props) {
-    super(props);
+import React from "react";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.components";
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+export const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    <CollectionsOverview />
+  </div>
+);
 
 export default ShopPage;
